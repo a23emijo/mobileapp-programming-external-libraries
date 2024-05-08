789f1fd81acb6c47f95fa0f99e37e04273002009
a23emijo <a23emijo@student.his.se>
2024-05-08 14:49:05 +0200

    Made the appliation able to load an image from the internet

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView picassoImage = findViewById(R.id.picassoImage);
         Picasso.get()
-                .load("")
+                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Windows_95_stacked_logo.svg/2560px-Windows_95_stacked_logo.svg.png")
+                .placeholder(R.mipmap.ic_launcher)
                 .error(R.drawable.error)
                 .into(picassoImage);
     }
